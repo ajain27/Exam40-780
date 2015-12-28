c25f221f165d5a7187a65de4239b32ecee4c2efe
ajain27 <a4ankit27@gmail.com>
2015-12-28 17:38:31 -0500

    Made changes to the HTML file and JS

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,11 +1,24 @@
 $(function (){
 
-	$friends = $("#friends");
+	$friends = $("#friends div li");
+	console.log($friends);
 	$name = $("#name");
 	$age = $("#age");
 
 
-	$("#editData").on("click", function (){
+	// $("#editData").on("click", function (){
+	// 	$li = $(this).closest('li');
+	// 	console.log($li);
+	// 	$li.find("input.name").val( $li.find("span.name") ).html();
+	// 	$li.addClass("edit");
+	// })
+
+	$(".cancelData").on("click", function (){
+		console.log("Cancelling...");
+		$(this).closest('li').removeClass("edit");
+	})
+
+	$friends.delegate('.editData', 'click', function (){
 		$li = $(this).closest('li');
 		console.log($li);
 		$li.find("input.name").val( $li.find("span.name") ).html();
