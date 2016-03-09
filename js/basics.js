f4ebdaf37962e4bf2aae3c61c6bfb19696533452
ajain27 <a4ankit27@gmail.com>
2016-03-09 14:58:00 -0800

    Calculating the area for a traingle

--- js/basics.js
+++ js/basics.js
@@ -39,3 +39,13 @@ function getDate () {
 
 	 document.getElementById("getDate").innerHTML = "Today is " +  m +  "-" + d +  "-" + year ;
 }
+
+function getArea() {
+	var side1 = 5;   
+	var side2 = 6;   
+	var side3 = 7;   
+	var perimeter = (side1 + side2 + side3)/2;  
+	var area =  Math.sqrt(perimeter*((perimeter-side1)*(perimeter-side2)*(perimeter-side3)));  
+	 document.getElementById("getArea").innerHTML = "Area is " + " " + area ;
+	
+}
