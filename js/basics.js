adeac1a14976ce66b51291f1ee3661a9a6d3b371
ajain27 <a4ankit27@gmail.com>
2016-03-09 14:03:42 -0800

    Getting the date in format required

--- js/basics.js
+++ js/basics.js
@@ -4,9 +4,11 @@ function todayDate () {
 	 var hours = today.getHours();
 	 var hoursIn12 = hours;
 	 var morEve = 'AM';
-	 if(hoursIn12 >=12 ) {
+	 if(hoursIn12 > 12 ) {
 	 	hoursIn12 = hours - 12;
 	 	morEve = "PM";
+	 } else if(hoursIn12 = 12){
+	 	// hoursIn12 == "00 AM"
 	 }
 	 var minutes = today.getMinutes();
 	 var seconds = today.getSeconds();
@@ -14,4 +16,26 @@ function todayDate () {
 	 document.getElementById("date").innerHTML = "Today is " +  weekDay[day] + " . ";
 	 document.getElementById("time").innerHTML = "Current time is " +  hoursIn12 +  " " + morEve +  " " + ":" +  " " + minutes + ":" +  " " +seconds;
 
+}
+
+function printThis () {
+	window.print();
+}
+
+function getDate () {
+ var today = new Date();
+ var date = today.getDate();
+ var month = today.getMonth() + 1;
+ var year = today.getFullYear();
+ var m = month;
+ if(m < 10) {
+ 	m = "0" + m;
+ }
+
+ var d = date;
+ if(d < 10) {
+ 	d = "0" + d;
+ }
+
+	 document.getElementById("getDate").innerHTML = "Today is " +  m +  "-" + d +  "-" + year ;
 }
